88e73fbe7a58e6b2c75b3928a951d75dbb400bd8
MeucheRoume <lucas29.david@gmail.com>
2021-03-15 00:21:58 +0100

    rendu final

--- build/build.js
+++ build/build.js
@@ -10,22 +10,21 @@ function draw() {
     fill('white');
     rect(0, 0, width, height);
     fill('black');
-    for (var i = 0; i < 5; i++) {
-        push();
-        randomSeed(i);
-        strokeWeight(5);
-        translate(pos / 2, 0, 0);
-        line(random(0, width), random(0, height / 2), random(0, width), random(0, height / 2));
-        pop();
-    }
-    for (var i = 0; i < 5; i++) {
-        push();
-        randomSeed(i);
-        strokeWeight(5);
-        translate(pos / 2, 0, 0);
-        line(width - random(0, width), height / 2 + (height - random(height / 2, height)), width - random(0, width), height / 2 + (height - random(height / 2, height)));
-        pop();
-    }
+    push();
+    strokeWeight(5);
+    translate(pos / 2, 0, 0);
+    line(0, 0, 5 * width / 6, height / 5);
+    line(width / 4, 0, width / 5, height / 2);
+    line(5 * width / 6, height / 5, width, height / 8);
+    line(5 * width / 6, height / 5, 4 * width / 5, height / 2);
+    line(5 * width / 6, 0, width, height / 16);
+    line(width / 4, height / 2, width / 5, 3 * height / 4);
+    line(width / 5, 3 * height / 4, 0, 13 * height / 16);
+    line(width / 5, 3 * height / 4, 5 * width / 6, height);
+    line(3 * width / 4, height / 2, 3 * width / 5, height);
+    line(0, 19 * height / 20, width / 10, height);
+    pos = 0;
+    pop();
 }
 function mouseWheel(event) {
     print(event.delta);
